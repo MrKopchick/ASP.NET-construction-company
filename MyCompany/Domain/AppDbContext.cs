dac81deb3e7208d75a91cdd87c3e3f85afff225c
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-02-24 17:15:53 +0200

    database context

---
+++ MyCompany/Domain/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyCompany.Domain.Entities;
+using System;
+
+
+namespace MyCompany.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<ServiceCategory> ServiceCategories { get; set; } = null!;
+        public DbSet<Service> Services { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            string adminName = "admin";
+            string userAdminId = Guid.NewGuid().ToString().ToUpper();
+            string roleAdminId = Guid.NewGuid().ToString().ToUpper();
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = roleAdminId,
+                Name = adminName,
+                NormalizedName = adminName.ToUpper()
+            });
+
+            builder.Entity<IdentityUser>().HasData(new IdentityUser
+            {
+                Id = userAdminId,
+                UserName = adminName,
+                NormalizedUserName = adminName.ToUpper(),
+                Email = "admin@admin.com",
+                NormalizedEmail= "admin@admin.com",
+                EmailConfirmed = true,
+                PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(new IdentityUser(), adminName),
+                SecurityStamp = string.Empty,
+                PhoneNumberConfirmed = true
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = roleAdminId,
+                UserId = userAdminId
+            });
+        }
+    }
+}
