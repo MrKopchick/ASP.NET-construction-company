c01f770b93ed4f5935988b314db654c61978be97
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-02-27 16:57:45 +0200

    Add Service repository interface and EF implementation

---
+++ MyCompany/Domain/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using MyCompany.Domain.Repositories.Abstract;
+
+namespace MyCompany.Domain
+{
+    public class DataManager
+    {
+        public IServiceCategoriesRepository ServiceCategories { get; set; }
+        public IServicesRepository Services { get; set; }   
+
+        public DataManager(IServiceCategoriesRepository servicesCategoriesRepository, IServicesRepository servicesRepository)
+        {
+            ServiceCategories = servicesCategoriesRepository;
+            Services = servicesRepository;
+        }
+
+    }
+}
---
+++ MyCompany/Domain/Repositories/Abstract/IServicesRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyCompany.Domain.Entities;
+
+namespace MyCompany.Domain.Repositories.Abstract
+{
+    public interface IServicesRepository
+    {
+        Task<IEnumerable<ServiceCategory>> GetServicesAsync();
+        Task<ServiceCategory?> GetServiceByIdAsync(int id);
+        Task SaveServiceAsync(ServiceCategory entity);
+        Task DeleteServiceAsync(int id);
+    }
+}
