03d16c8210c390c470530c78c955f33c27803d0d
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-02-27 16:58:57 +0200

    Fix ServiceCategories repository interface

--- MyCompany/Domain/Repositories/Abstract/IServicesRepository.cs
+++ MyCompany/Domain/Repositories/Abstract/IServicesRepository.cs
@@ -1,12 +1,13 @@
 ﻿using MyCompany.Domain.Entities;
+using MyCompany.Domain.Enums;
 
 namespace MyCompany.Domain.Repositories.Abstract
 {
     public interface IServicesRepository
     {
-        Task<IEnumerable<ServiceCategory>> GetServicesAsync();
-        Task<ServiceCategory?> GetServiceByIdAsync(int id);
-        Task SaveServiceAsync(ServiceCategory entity);
+        Task<IEnumerable<Service>> GetServicesAsync();
+        Task<Service?> GetServiceByIdAsync(int id);
+        Task SaveServiceAsync(Service entity);
         Task DeleteServiceAsync(int id);
     }
 }
