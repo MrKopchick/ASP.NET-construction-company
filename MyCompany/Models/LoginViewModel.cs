084591af7e5f8d1c75f43eefc2c4e9bdd6f05117
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-01 21:03:37 +0200

    Add login model

---
+++ MyCompany/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string? Username { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
