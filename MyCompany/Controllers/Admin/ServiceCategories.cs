825084a0c80853bff011496ff809bd0f029a15d6
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-03 17:53:27 +0200

    Admin Controller. ServiceCategories

--- MyCompany/Controllers/Admin/ServiceCategories.cs
+++ MyCompany/Controllers/Admin/ServiceCategories.cs
@@ -1,7 +1,31 @@
-﻿namespace MyCompany.Controllers.Admin
+﻿using MyCompany.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCompany.Controllers.Admin
 {
     public partial class AdminController
     {
+        public async Task<IActionResult> ServiceCategoriesEdit(int id){
+            ServiceCategory? entity = id == default 
+                ? new ServiceCategory() 
+                : await _dataManager.ServiceCategories.GetServiceCategoryByIdAsync(id);
 
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServiceCategoriesEdit(ServiceCategory entity){
+            if(!ModelState.IsValid) return View(entity);
+
+            await _dataManager.ServiceCategories.SaveServiceCategoryAsync(entity);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServiceCategoriesDelete(int id){
+            await _dataManager.ServiceCategories.DeleteServiceCategoryAsync(id);
+            return RedirectToAction("Index");
+        }
     }
 }
