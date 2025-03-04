87c198aa9aa92efbf1da98381d04d22cb2133822
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-04 17:26:27 +0200

    Admin controller. Service part

--- MyCompany/Controllers/Admin/Services.cs
+++ MyCompany/Controllers/Admin/Services.cs
@@ -1,6 +1,41 @@
-﻿namespace MyCompany.Controllers.Admin
+﻿using MyCompany.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+
+namespace MyCompany.Controllers.Admin
 {
     public partial class AdminController
     {
+        public async Task<IActionResult> ServicesEdit(int id){
+            Service? entity = id == default ? new Service() : await _dataManager.Services.GetServiceByIdAsync(id);
+            ViewBag.ServiceCategories = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServicesEdit(Service entity, IFormFile? titleImageFile){
+            if(!ModelState.IsValid){
+                ViewBag.ServiceCategories = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
+                return View(entity);
+            }
+
+            if(titleImageFile != null){
+                entity.Photo = titleImageFile.FileName;
+                await SaveImg(titleImageFile);
+            }
+
+            await _dataManager.Services.SaveServiceAsync(entity);
+
+            return RedirectToAction("Index");
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> ServicesDelete(int id){
+            await _dataManager.Services.DeleteServiceAsync(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
