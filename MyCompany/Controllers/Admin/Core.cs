e2a05667194fea5db9208831b88c22961f418501
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-04 17:26:41 +0200

    saveImg

--- MyCompany/Controllers/Admin/Core.cs
+++ MyCompany/Controllers/Admin/Core.cs
@@ -8,17 +8,27 @@ namespace MyCompany.Controllers.Admin
     public partial class AdminController : Controller
     {
         private readonly DataManager _dataManager;
+        private readonly IWebHostEnvironment _hostingEnviroment;
 
-        public AdminController(DataManager dataManager)
+        public AdminController(DataManager dataManager, IWebHostEnvironment hostingEnviroment)
         {
             _dataManager = dataManager;
-
+            _hostingEnviroment = hostingEnviroment;
         }
         public async Task<IActionResult> Index()
         {
             ViewBag.ServiceCategories = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
             ViewBag.Services = await _dataManager.Services.GetServicesAsync();
             return View(); 
+        }
+
+        public async Task<string> SaveImg(IFormFile img)
+        {
+            string path = Path.Combine(_hostingEnviroment.WebRootPath, "img/", img.FileName);
+            await using FileStream stream = new FileStream(path, FileMode.Create);
+            await img.CopyToAsync(stream);
+
+            return path;
         }
     }
 }
