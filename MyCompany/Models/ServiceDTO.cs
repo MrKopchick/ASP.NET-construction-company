7855b26d332b1a2b1b93643cd39a75276ca16a74
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-04 19:15:52 +0200

    Service DTO

---
+++ MyCompany/Models/ServiceDTO.cs
@@ -0,0 +1,11 @@
+namespace MyCompany.Models{
+    public class ServiceDTO{
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Title { get; set; }
+        public string? DescriptionShort { get; set; }
+        public string? Description { get; set; }
+        public string? PhotoFileName { get; set; }
+        public string? Type { get; set; }
+    }
+}
