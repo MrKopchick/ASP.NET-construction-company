9ee822730267f9cbfcf365d00fb6a5080fa8467b
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-04 22:31:18 +0200

    ServiceController

---
+++ MyCompany/Controllers/ServicesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain.Entities;
+using MyCompany.Infrastructure;
+using MyCompany.Models;
+using MyCompany.Domain;
+
+namespace MyCompany.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager _dataManager;
+        public ServicesController(DataManager dataManager){
+            _dataManager = dataManager;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Service> list = await _dataManager.Services.GetServicesAsync();
+
+            IEnumerable<ServiceDTO> listDTO = HelperDTO.TransformServices(list);
+            return View(listDTO);
+        }
+
+        public async Task<IActionResult> Show(int id){
+            Service? entity = await _dataManager.Services.GetServiceByIdAsync(id);
+
+            if(entity is null){
+                return NotFound();  
+            }
+
+            ServiceDTO dto = HelperDTO.TransformService(entity);
+
+            return View(dto);
+        }
+    }
+}
